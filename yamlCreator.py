9637f833e98fd1f630ede0883a8913c379cfad16
Koehorst, Jasper <jasper.koehorst@wur.nl>
2016-06-05 18:07:52 +0200

    test function added

--- yamlCreator.py
+++ yamlCreator.py
@@ -80,7 +80,46 @@ default
 	print(command)
 	os.system(command)
 
+def test():
+	arguments["fastq"] = os.path.abspath("myReadFolder/reads.fq.gz")
+	arguments["output"] = os.path.abspath("test")
+	arguments["yaml"] = arguments["output"]+"/biobox.yaml"
+	#IF output folder does not exist yet...
+	if not os.path.isdir(arguments["output"]):
+		os.mkdir(arguments["output"])
+	arguments["id"] = "TEST"
+	arguments["fasta"] = "myTestResult.fa"
+
+
+	assemblers = ["bioboxes/velvet",
+				  "bioboxes/megahit",
+				  "bioboxes/sga",
+				  "bioboxes/idba",
+				  "bioboxes/minia",
+				  "bioboxes/ray",
+				  "bioboxes/sparse",
+				  "bioboxes/soap"]
+
+	for assembler in assemblers:
+		arguments["assembler"] = assembler
+		arguments["type"] = "single"
+		assemblerSelection()
+		yamlCreator()
+		runDocker()
+
+	for assembler in assemblers:
+		arguments["assembler"] = assembler
+		arguments["type"] = "paired"
+		assemblerSelection()
+		yamlCreator()
+		runDocker()
+
+
 if __name__ == '__main__':
+	if "--test" in sys.argv:
+		print("PERFORMING TESTS")
+		test()
+
 	if "--fastq" not in sys.argv:
 		print("--fastq FASTQ required")
 		sys.exit()
@@ -114,4 +153,11 @@ if __name__ == '__main__':
 	assemblerSelection()
 	yamlCreator()
 	runDocker()
+
+	#If the output fasta file is required to be moved to somewhere else -ofasta is used
+	if "--fasta" in sys.argv:
+		arguments["fasta"] = sys.argv[sys.argv.index("--fasta")+1]
+		import shutil
+		shutil.move(arguments["output"]+"/contig.fa", arguments["fasta"])
+
 
